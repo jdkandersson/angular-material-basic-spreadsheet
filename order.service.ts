e73d1b9c1e2f75c1167174c255917cb5fa3fade3
jdkandersson <51036209+jdkandersson@users.noreply.github.com>
2019-06-02 10:45:34 +1000

    Create order.service.ts

---
+++ order.service.ts
@@ -0,0 +1,36 @@
+// order.service.ts
+import { Injectable } from '@angular/core';
+
+import { Order } from './order.model';
+import { OrderTable } from './order-table.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor() { }
+
+  getOrderTable(): OrderTable {
+    const orders: Order[] = [
+      {date: '2000-01-01', product: 'Apples', value: 1000},
+      {date: '2000-02-01', product: 'Apples', value: 2000},
+      {date: '2000-01-01', product: 'Oranges', value: 10000}
+    ];
+    return {
+      headers: ['2000-01-01', '2000-02-01'],
+      rowLabels: ['Apples', 'Oranges'],
+      orders: orders,
+      orderMap: {
+        '2000-01-01': {
+          'Apples': orders[0],
+          'Oranges': orders[2]
+        },
+        '2000-02-01': {
+          'Apples': orders[1],
+          'Oranges': orders[3]
+        }
+      }
+    };
+  }
+}
